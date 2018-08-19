516fd719300dfb460fef2bf7e70d2f57eea20887
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-20 01:52:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -127,7 +127,7 @@ if(cmd === `${prefix}ban`) {
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
     
-    await member.ban(reason)
+message.member.ban(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
