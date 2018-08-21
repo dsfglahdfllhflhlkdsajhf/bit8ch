0c3b876880d2e4b3be88ffd16df8b392fe75b4ab
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-21 17:18:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ message.guild.unban(user);
 let eeeeeeeee = new Discord.RichEmbed()
 .setAuthor(`UNBANNED!`)
 .setColor("RANDOM")
-.addField("** Uesr:**",  '**[ ' + `${user.tag}` + ' ]**')
+ .addField('**●User Unban :** ', `${user}` , true)
 .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
 message.channel.sendEmbed(eeeeeeeee)
 
