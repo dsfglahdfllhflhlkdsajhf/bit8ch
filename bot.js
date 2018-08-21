9b6165eea0f1a9444ac305c1e3331404b15c52c3
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-21 17:22:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,14 +78,20 @@ embed : bbbbbb
     break;
 
 case "unban":
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("U don't have enough permissions to **Unban members** :lol:");
-message.guild.unban(user);
-let eeeeeeeee = new Discord.RichEmbed()
-.setAuthor(`UNBANNED!`)
-.setColor("RANDOM")
- .addField('**●User Unban :** ', `${user}` , true)
-.addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-message.channel.sendEmbed(eeeeeeeee)
+  let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
+  if(message.content.startsWith(prefix + 'unban')) {
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
+      if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
+      message.guild.unban(user);
+      message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
+      var embed = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURl)
+      .setColor("RANDOM")
+      .setTitle('**Unban** !')
+      .addField('**User Unban :** ', `${user}` , true)
+      .addField('**By :**' ,       ` <@${message.author.id}> ` , true)
+      .setAuthor(message.guild.name)
+      message.channel.sendEmbed(embed)
 
   
   break;
