81c34bd50fae13d7a3e141b7cc42106f97a42622
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-21 17:31:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,20 +78,18 @@ embed : bbbbbb
     break;
 
 case "unban":
-  let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
+  let userr = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
   if(message.content.startsWith(prefix + 'unban')) {
       if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
-      if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
-      message.guild.unban(user);
-      message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
-      var embed = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURl)
+      if(!userr) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
+      message.guild.unban(userr);
+      var embedddd = new Discord.RichEmbed()
+      .setAuthor(userr.displayAvatarURL, "UNBAN")
       .setColor("RANDOM")
-      .setTitle('**Unban** !')
-      .addField('**User Unban :** ', `${user}` , true)
+      .addField('**User Unban :** ', `${userr}` , true)
       .addField('**By :**' ,       ` <@${message.author.id}> ` , true)
       .setAuthor(message.guild.name)
-      message.channel.sendEmbed(embed)
+      message.channel.sendEmbed(embedddd)
 
   
   break;
